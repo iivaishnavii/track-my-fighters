608f2e6a2aaf956e4b427378023a1f608633d976
Vaishnavi Ramesh <vramesh@pulsesecure.net>
2020-05-06 20:42:43 -0700

    Front end login

---
+++ sifrontend/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div class="form">
+            <ul class="tab-group">
+                <li class="tab active"><a href="#login">Log In</a></li>
+                <li class="tab"><a href="#signup">Sign Up</a></li>
+            </ul>
+    </div>
+  );
+}
+
+export default Login;
--- sifrontend/src/components/Map.js
+++ sifrontend/src/components/Map.js
@@ -23,10 +23,10 @@ function MapComp(){
       />
       {selectedLocation && (
         <InfoWindow
-        position={{lat: selectedLocation.geometry.coordinates[0],lng:selectedLocation.geometry.coordinates[1]}}
-        onCloseClick={() => {
-          setSelectedLocation(null)
-        }
+            position={{lat: selectedLocation.geometry.coordinates[0],lng:selectedLocation.geometry.coordinates[1]}}
+            onCloseClick={() => {
+                setSelectedLocation(null)
+           }
         }
         >
           <div>
@@ -42,14 +42,17 @@ function MapComp(){
 function Map() {
 
   return (
-    <div style={{width : '100%', height : '100%'}}>
-        
-        <WrappedMap
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCw3PUt4Smi1VpUlU-84q9MEQICOzRp2Hs"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-        />
+    <div className="container">
+
+      <div className="row" style={{'bottom':'0px'}}>
+      <WrappedMap
+      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCw3PUt4Smi1VpUlU-84q9MEQICOzRp2Hs"
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: `700px` }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+      />
+      </div>
+      
     </div>
   );
 }
