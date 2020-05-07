618fdbed59244a888d3248d797d9e470b0135b8b
Vaishnavi Ramesh <vramesh@pulsesecure.net>
2020-05-06 21:33:17 -0700

    Login

--- sifrontend/src/App.js
+++ sifrontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from './components/Nav'
 import Details from './components/Details'
 import Map from './components/Map'
+import Login from './components/Login'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 
@@ -15,6 +16,7 @@ function App() {
           <Switch>
             <Route path="/map" exact component={Map}></Route>
             <Route path="/details" exact component={Details}></Route>
+            <Route path="/login" exact component={Login}></Route>
           </Switch>
        
       </div>
--- sifrontend/src/components/Login.js
+++ sifrontend/src/components/Login.js
@@ -7,6 +7,85 @@ function Login() {
                 <li class="tab active"><a href="#login">Log In</a></li>
                 <li class="tab"><a href="#signup">Sign Up</a></li>
             </ul>
+
+            <div class="tab-content">
+              <div id="login">   
+                  <h1>Captain login</h1>
+              
+                  <form action="/" method="post">
+              
+                      <div class="field-wrap">
+                          <label>User name<span class="req">*</span>
+                          </label>
+                          <input type="text"required autocomplete="off"/>
+                      </div>
+              
+                      <div class="field-wrap">
+                        <label>
+                          Password<span class="req">*</span>
+                        </label>
+                        <input type="password"required autocomplete="off"/>
+                      </div>
+              
+                      <p class="forgot"><a href="#">Forgot Password?</a></p>
+              
+                     <Button class="button button-block">Log In</Button>Log In</button>
+              
+                  </form>
+              </div>
+            </div>
+            
+            <div id="signup">   
+              <h1>New Captains! Register here</h1>
+              
+              <form action="/" method="post">
+              
+              <div class="top-row">
+                <div class="field-wrap">
+                  <label>
+                    First Name<span class="req">*</span>
+                  </label>
+                  <input type="text" required autocomplete="off" />
+                </div>
+            
+                <div class="field-wrap">
+                  <label>
+                    Last Name<span class="req">*</span>
+                  </label>
+                  <input type="text"required autocomplete="off"/>
+                </div>
+              </div>
+              
+              <div class="field-wrap">
+                <label>
+                  Squad Name<span class="req">*</span>
+                </label>
+                <input type="text"required autocomplete="off"/>
+              </div>
+    
+              <div class="field-wrap">
+                <label>
+                  User Name<span class="req">*</span>
+                </label>
+                <input type="text"required autocomplete="off"/>
+              </div>
+              
+              <div class="field-wrap">
+                <label>
+                  Set A Password<span class="req">*</span>
+                </label>
+                <input type="password"required autocomplete="off"/>
+              </div>
+              
+              <button type="submit" class="button button-block"/>Register</button>
+              
+              </form>
+    
+            </div>
+            
+          </div><!-- tab-content -->
+          
+    </div>
     </div>
   );
 }
